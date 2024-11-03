932359c3cc46859501c055724dfd0f7fa2773bb6
userhhhhh <hqs123@sjtu.edu.cn>
2024-11-03 19:02:53 +0800

    test

--- src/cpu.v
+++ src/cpu.v
@@ -1,6 +1,7 @@
 // RISCV32 CPU top module
 // port modification allowed for debugging purposes
 
+
 module cpu(
   input  wire                 clk_in,			// system clock signal
   input  wire                 rst_in,			// reset signal
